1cae60e12bc4b80af42e565c4b9ac937eeb1865f
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-12-13 19:48:36 +0300

    Подключние к бд

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,25 @@
-import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Post, Put, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
-
-  constructor(private readonly productService: ProductService) {
-
-  }
+  constructor(private readonly productService: ProductService) {}
 
   // @Get()
   // getAll(@Req() req: Request, @Res() res: Response): string {
@@ -16,29 +27,32 @@ export class ProductsController {
   // }
 
   @Get()
-  getAll() {
+  getAll(): Promise<Product[]> {
     return this.productService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id): string {
+  getOne(@Param('id') id): Promise<Product> {
     return this.productService.getById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-control', 'none')
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productService.create(createProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return 'Remove' + id;
+  remove(@Param('id') id: string): Promise<Product> {
+    return this.productService.remove(id);
   }
 
-  @Put(':id') 
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
-    return id +': ' + updateProductDto.title + ' ' + updateProductDto.price
+  @Put(':id')
+  update(
+    @Body() updateProductDto: UpdateProductDto,
+    @Param('id') id: string,
+  ): Promise<Product> {
+    return this.productService.update(id, updateProductDto);
   }
 }
