edb5a4edbde1bd3b159401eea207e0153413d80a
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-12-21 23:03:22 +0300

    cors v2

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
+    origin: true,
   });
   await app.listen(process.env.PORT || 3000);
 }
