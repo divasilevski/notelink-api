e961f3b61488f4f27ce33a49dd3d155dbc743127
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-01-03 14:47:41 +0300

    Изменены заголовки

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -4,10 +4,17 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 export class AppMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     let allowedOrigins = ['http://localhost:3000'];
+    console.log(req.header('Origin'));
     if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+      console.log('no Cors');
       res.header('Access-Control-Allow-Origin', req.header('Origin'));
-      res.header('Access-Control-Allow-Headers', 'content-type');
-      res.header('Access-Control-Allow-Methods', 'POST');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+      );
+
+      res.header('Content-Type', 'application/json');
     }
     next();
   }
